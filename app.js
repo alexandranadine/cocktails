0577043984bde347be9ab3de82beff314419779e
Alexandra Lewis <alexandra.nadine.lewis@gmail.com>
2022-04-30 11:47:37 -0700

    Changed logo

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
+
+// ------------------INTERSECTION OBSERVER - CHANGES NAVBAR ON SCROLL
+
 const header = document.querySelector("header");
 const heroSection = document.querySelector(".hero");
-const faders = document.querySelectorAll(".fade-in");
 const navOptions = {};
-const appearOptions = {
-    threshold: 1,
-    rootMargin: '0px 0px 100px 0px'
-};
 
 const navObserver = new IntersectionObserver(function (entries, navObserver) {
   entries.forEach((entry) => {
@@ -18,6 +16,14 @@ const navObserver = new IntersectionObserver(function (entries, navObserver) {
 }, navOptions);
 
 navObserver.observe(heroSection);
+
+// ------------------INTERSECTION OBSERVER - CARDS FADE IN
+
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = {
+  threshold: 1,
+  rootMargin: '0px 0px 100px 0px'
+};
 
 const appearOnScroll = new IntersectionObserver(function (
   entries,
@@ -37,3 +43,42 @@ appearOptions);
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 });
+
+// ------------------------------MODAL POPUPS
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.querySelector('#overlay');
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach(modal => {
+    closeModal(modal);
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal =button.closest('.modal')
+    closeModal(modal);
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
